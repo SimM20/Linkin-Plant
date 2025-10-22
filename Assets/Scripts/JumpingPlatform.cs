1889eb9a4f971f9512ce91ee8a7fe121054ca647
warreR33 <154948249+warreR33@users.noreply.github.com>
2025-10-22 11:12:29 -0300

    Rebote?

--- Assets/Scripts/JumpingPlatform.cs
+++ Assets/Scripts/JumpingPlatform.cs
@@ -1,9 +1,36 @@
 using UnityEngine;
+using System.Collections;
 
-public class JumpingPlatform : CustomBehaviour
+public class JumpingPlatform : MonoBehaviour
 {
+    [SerializeField] private float jumpForce = 8f;
+    [SerializeField] private float jumpDuration = 0.25f;
+
     private void OnTriggerEnter(Collider other)
     {
-        other.transform.Translate(new Vector3(0, 2f, 0));
+        var controller = other.GetComponent<CharacterController>();
+        if (controller != null)
+        {
+            StartCoroutine(ApplyImpulse(controller));
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        var controller = other.GetComponent<CharacterController>();
+        if (controller != null)
+            controller.Move(Vector3.up * jumpForce * Time.deltaTime);
+    }
+
+
+    private IEnumerator ApplyImpulse(CharacterController controller)
+    {
+        float timer = 0f;
+        while (timer < jumpDuration)
+        {
+            controller.Move(Vector3.up * jumpForce * Time.deltaTime);
+            timer += Time.deltaTime;
+            yield return null;
+        }
     }
 }
